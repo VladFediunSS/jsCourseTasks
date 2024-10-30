58525a512ac1d32560d4c3caddc454eb1efa40a2
Fediun <fediun.work@gmail.com>
2024-10-30 13:57:33 +0100

    Last cahnge

--- hw1_task6.js
+++ hw1_task6.js
@@ -1,31 +1,41 @@
 function primeCheck(value){
-    const squareRoot = Math.sqrt(value);
+    if (valueCheck(value)){
+        const squareRoot = Math.sqrt(value);
     
-    const checkList = [];
-    for (let i = 2; i <= Math.floor(squareRoot); i++) {
-        checkList.push(i);}
-
-    for (i of checkList){
-        const rest = value % i;
-        console.log(`value: ${value}, rest: ${rest}`);
-        if (rest === 0){
-            console.log(`${value} is NOT a prime number.`);
+        if (squareRoot < 2) {
+            console.log(`${value} is a prime number.`)
             return;
         }
-        }
-        console.log(`${value} is a prime number.`);
-        return;
+
+        const checkList = [];
+        for (let i = 2; i <= Math.floor(squareRoot); i++) {
+            checkList.push(i);}
+
+        for (i of checkList){
+            const rest = value % i;
+            //console.log(`value: ${value}, rest: ${rest}`);
+            if (rest === 0){
+                console.log(`${value} is NOT a prime number.`);
+                return;
+            }
+            }
+            console.log(`${value} is a prime number.`);
+            return;
+    } else {
+        console.log('Incorrect input!')
+    }
+    
     }
 
 function valueCheck(value){
-    if (value === Math.floor(value)){
+    if (value === Math.floor(value) && value > 0){
         return true;
     } else {
         return false;
     }
 }
 
-const scores = [0, 2, 3, 5, 7, 8, 11, 13, 15, 19, 37, 71, 87, 88, 99, 100, 101];
+const scores = [-1, 0, 0.5, 1, 2, 3, 4, 5, 7, 8, 11, 13, 15, 19, 37, 71, 87, 88, 99, 100, 101];
 
 for (i of scores){
     primeCheck(i);
